a9b1cb0c63e42e74ed3a3cb036436e78e1d60631
Ko2doo <ko2doodev@gmail.com>
2020-08-26 12:37:13 +0500

    UPD 26.08

--- src/component/dialogs/Dialogs.jsx
+++ src/component/dialogs/Dialogs.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import MemberCard from '../chat-card/Member-card';
 import MessageCard from '../message-card/Message-card';
-import { BrowserRouter, NavLink } from 'react-router-dom';
+// import { BrowserRouter, NavLink } from 'react-router-dom';
 
 //import icon
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -15,28 +15,66 @@ import avatar2 from '../details/img/avatar2.png';
 
 
 const Dialogs = (props) => {
+
+  let dialogsData = [
+    {
+      id: 1,
+      avatarURL: {avatar},
+      memberName: "Joe",
+      lastTimeMessage: "18:04",
+      memberStatus: "online"
+    },
+    {
+      id: 2,
+      avatarURL: {avatar2},
+      memberName: "John Cena",
+      lastTimeMessage: "14:48",
+      memberStatus: "offline"
+    }
+  ];
+
+  let messagesData = [
+    {
+      id: 1,
+      avatarURL: {avatar},
+      authorName: "Me",
+      message: "Hi John!"
+    },
+    {
+      id: 2,
+      avatarURL: {avatar2},
+      authorName: "John Cena",
+      message: "Hi my dear friend! How are you?"
+    },    
+    {
+      id: 3,
+      avatarURL: {avatar},
+      authorName: "Me",
+      message: "Nice, John"
+    }
+  ];
+
   return (
-    <BrowserRouter>
       <React.Fragment>
         <section className={style.members_dialog}>
           <ul className={style.dialogs__list}>
             <li className={style.list__item}>
               <MemberCard
-                id="1"
-                avatarURL={avatar}
-                memberName="Joe"
-                lastTimeMessage="18:04"
-                memberStatus="online"
+                id={dialogsData[0].id}
+                avatarURL={dialogsData[0].avatarURL}
+                memberName={dialogsData[0].memberName}
+                lastTimeMessage={dialogsData[0].lastTimeMessage}
+                memberStatus={dialogsData[0].memberStatus}
               />
             </li>
 
             <li className={style.list__item}>
               <MemberCard
-                id="2"
-                avatarURL={avatar2}
-                memberName="John Cena"
-                lastTimeMessage="14:48"
-                memberStatus="offline"
+                id={dialogsData[1].id}
+                avatarURL={dialogsData[1].avatarURL}
+                memberName={dialogsData[1].memberName}
+                lastTimeMessage={dialogsData[1].lastTimeMessage}
+                memberStatus={dialogsData[1].memberStatus}
               />
             </li>
           </ul>
@@ -44,27 +82,29 @@ const Dialogs = (props) => {
           <div className={style.dialogs__messages}>
 
             <MessageCard
-              avatarURL={avatar}
-              authorName="Me"
-              message="Hi John!"
+              id={messagesData[0].id}
+              avatarURL={messagesData[0].avatarURL}
+              authorName={messagesData[0].authorName}
+              message={messagesData[0].message}
             />
 
             <MessageCard
-              avatarURL={avatar2}
-              authorName="John Cena"
-              message="Hi my dear friend! How are you?"
+              id={messagesData[1].id}
+              avatarURL={messagesData[1].avatarURL}
+              authorName={messagesData[1].authorName}
+              message={messagesData[1].message}
             />
 
             <MessageCard
-              avatarURL={avatar}
-              authorName="Me"
-              message="Nice, John"
+              id={messagesData[2].id}
+              avatarURL={messagesData[2].avatarURL}
+              authorName={messagesData[2].authorName}
+              message={messagesData[2].message}
             />
 
           </div>
         </section>
       </React.Fragment>
-    </BrowserRouter>
   );
 }
 
--- src/component/posts/Posts.jsx
+++ src/component/posts/Posts.jsx
@@ -6,10 +6,48 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import style from './Posts.module.scss';
 
 const Posts = (props) => {
+
+  let messagesData = [
+    {
+      id: 1,
+      message: "Привет как дела?",
+      likedCount: "25",
+      dislikedCount: "3"
+    },
+    {
+      id: 2,
+      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolore necessitatibus maiores nam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolore necessitatibus maiores nam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolore necessitatibus maiores nam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolore necessitatibus maiores nam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolore necessitatibus maiores nam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolore necessitatibus maiores nam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolore necessitatibus maiores nam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolore necessitatibus maiores nam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolore necessitatibus maiores nam?",
+      likedCount: "35",
+      dislikedCount: "2"
+    },
+    {
+      id: 3,
+      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolore necessitatibus maiores nam?",
+      likedCount: "3",
+      dislikedCount: "300"
+    }
+  ];
+
   return (
     <React.Fragment>
-      <Card message='Привет как дела?' likedCount="3" dislikedCount="1" />
-      <Card message='Мой первый пост' likedCount="25" dislikedCount="8" />
+      <Card
+        id={messagesData[0].id}
+        message={messagesData[0].message}
+        likedCount={messagesData[0].likedCount}
+        dislikedCount={messagesData[0].dislikedCount}
+      />
+      <Card
+        id={messagesData[1].id}
+        message={messagesData[1].message}
+        likedCount={messagesData[1].likedCount}
+        dislikedCount={messagesData[1].dislikedCount}
+      />
+      <Card
+        id={messagesData[2].id}
+        message={messagesData[2].message}
+        likedCount={messagesData[2].likedCount}
+        dislikedCount={messagesData[2].dislikedCount}
+      />
 
       <div className={style.write_post}>
         <textarea className={style.textarea}>
